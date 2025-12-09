7ea969f0552e072af1443f4a379ebd465f0804b9
Caspar Kroll <ckroll@schulzentrum.de>
2025-12-09 15:28:13 +0100

    Added random color change V1.0.1

--- js/js.js
+++ js/js.js
@@ -184,25 +184,117 @@ function update() {
 }
 
 function colorChange() {
-    mapColorArrayToGroups([
-        { color: pinkColors, groupID: "roof" },
-        { color: redColors, groupID: "roof #divider" },
-        { color: yellowColors, groupID: "roof #top-front" },
-        { color: blueColors, groupID: "roof #roof-divider" },
-        { color: pinkColors, groupID: "a3-og-bg" },
-        { color: tomatoColors, groupID: "aula-headstones" },
-        { color: redColors, groupID: "aula-top-bg" },
-        { color: tomatoColors, groupID: "aula-top-bg-stroke" },
-        { color: blueColors, groupID: "aula-awening" },
-        { color: yellowColors, groupID: "aula-divider" },
-        { color: limeColors, groupID: "aula-under" },
-        { color: greenColors, groupID: "a2-3-divider" },
-        { color: blueColors, groupID: "a2-og-awening" },
-        { color: yellowColors, groupID: "a2-og" },
-        { color: pinkColors, groupClass: "a1-og-awening-small" },
-        { color: tomatoColors, groupClass: "a1-og-awening" },
-        { color: greenColors, groupID: "a1-og-bg" },
-        { color: blueColors, groupID: "windows" }
-    ]);
+    switch (Math.floor(Math.random() * 4)) {
+        case 0:
+            mapColorArrayToGroups([
+                { color: pinkColors, groupID: "roof" },
+                { color: redColors, groupID: "roof #divider" },
+                { color: yellowColors, groupID: "roof #top-front" },
+                { color: blueColors, groupID: "roof #roof-divider" },
+                { color: pinkColors, groupID: "a3-og-bg" },
+                { color: tomatoColors, groupID: "aula-headstones" },
+                { color: redColors, groupID: "aula-top-bg" },
+                { color: tomatoColors, groupID: "aula-top-bg-stroke" },
+                { color: blueColors, groupID: "aula-awening" },
+                { color: yellowColors, groupID: "aula-divider" },
+                { color: limeColors, groupID: "aula-under" },
+                { color: greenColors, groupID: "a2-3-divider" },
+                { color: blueColors, groupID: "a2-og-awening" },
+                { color: yellowColors, groupID: "a2-og" },
+                { color: pinkColors, groupClass: "a1-og-awening-small" },
+                { color: tomatoColors, groupClass: "a1-og-awening" },
+                { color: greenColors, groupID: "a1-og-bg" },
+                { color: blueColors, groupID: "windows" }
+            ]);
+            break;
+        case 1:
+            mapColorArrayToGroups([
+                { color: redColors, groupID: "roof" },
+                { color: yellowColors, groupID: "roof #divider" },
+                { color: blueColors, groupID: "roof #top-front" },
+                { color: pinkColors, groupID: "roof #roof-divider" },
+                { color: tomatoColors, groupID: "a3-og-bg" },
+                { color: redColors, groupID: "aula-headstones" },
+                { color: tomatoColors, groupID: "aula-top-bg" },
+                { color: blueColors, groupID: "aula-top-bg-stroke" },
+                { color: yellowColors, groupID: "aula-awening" },
+                { color: limeColors, groupID: "aula-divider" },
+                { color: greenColors, groupID: "aula-under" },
+                { color: blueColors, groupID: "a2-3-divider" },
+                { color: yellowColors, groupID: "a2-og-awening" },
+                { color: pinkColors, groupID: "a2-og" },
+                { color: tomatoColors, groupClass: "a1-og-awening-small" },
+                { color: greenColors, groupClass: "a1-og-awening" },
+                { color: blueColors, groupID: "a1-og-bg" },
+                { color: pinkColors, groupID: "windows" }
+            ]);
+            break;
+        case 2:
+            mapColorArrayToGroups([
+                { color: yellowColors, groupID: "roof" },
+                { color: blueColors, groupID: "roof #divider" },
+                { color: pinkColors, groupID: "roof #top-front" },
+                { color: tomatoColors, groupID: "roof #roof-divider" },
+                { color: redColors, groupID: "a3-og-bg" },
+                { color: tomatoColors, groupID: "aula-headstones" },
+                { color: blueColors, groupID: "aula-top-bg" },
+                { color: yellowColors, groupID: "aula-top-bg-stroke" },
+                { color: limeColors, groupID: "aula-awening" },
+                { color: greenColors, groupID: "aula-divider" },
+                { color: blueColors, groupID: "aula-under" },
+                { color: yellowColors, groupID: "a2-3-divider" },
+                { color: pinkColors, groupID: "a2-og-awening" },
+                { color: tomatoColors, groupID: "a2-og" },
+                { color: greenColors, groupClass: "a1-og-awening-small" },
+                { color: blueColors, groupClass: "a1-og-awening" },
+                { color: pinkColors, groupID: "a1-og-bg" },
+                { color: redColors, groupID: "windows" }
+            ]);
+            break;
+        case 3:
+            mapColorArrayToGroups([
+                { color: blueColors, groupID: "roof" },
+                { color: pinkColors, groupID: "roof #divider" },
+                { color: tomatoColors, groupID: "roof #top-front" },
+                { color: redColors, groupID: "roof #roof-divider" },
+                { color: tomatoColors, groupID: "a3-og-bg" },
+                { color: blueColors, groupID: "aula-headstones" },
+                { color: yellowColors, groupID: "aula-top-bg" },
+                { color: limeColors, groupID: "aula-top-bg-stroke" },
+                { color: greenColors, groupID: "aula-awening" },
+                { color: blueColors, groupID: "aula-divider" },
+                { color: yellowColors, groupID: "aula-under" },
+                { color: pinkColors, groupID: "a2-3-divider" },
+                { color: tomatoColors, groupID: "a2-og-awening" },
+                { color: greenColors, groupID: "a2-og" },
+                { color: blueColors, groupClass: "a1-og-awening-small" },
+                { color: pinkColors, groupClass: "a1-og-awening" },
+                { color: redColors, groupID: "a1-og-bg" },
+                { color: yellowColors, groupID: "windows" }
+            ]);
+            break;
+        case 4:
+            mapColorArrayToGroups([
+                { color: pinkColors, groupID: "roof" },
+                { color: tomatoColors, groupID: "roof #divider" },
+                { color: redColors, groupID: "roof #top-front" },
+                { color: tomatoColors, groupID: "roof #roof-divider" },
+                { color: blueColors, groupID: "a3-og-bg" },
+                { color: yellowColors, groupID: "aula-headstones" },
+                { color: limeColors, groupID: "aula-top-bg" },
+                { color: greenColors, groupID: "aula-top-bg-stroke" },
+                { color: blueColors, groupID: "aula-awening" },
+                { color: yellowColors, groupID: "aula-divider" },
+                { color: pinkColors, groupID: "aula-under" },
+                { color: tomatoColors, groupID: "a2-3-divider" },
+                { color: greenColors, groupID: "a2-og-awening" },
+                { color: blueColors, groupID: "a2-og" },
+                { color: pinkColors, groupClass: "a1-og-awening-small" },
+                { color: redColors, groupClass: "a1-og-awening" },
+                { color: yellowColors, groupID: "a1-og-bg" },
+                { color: blueColors, groupID: "windows" }
+            ]);
+            break;
+    }
     mapTreeColors(treeColors, "oklch(0.2255 0.0549 145)");
 }
